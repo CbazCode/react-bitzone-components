9604df55daf9135e112154151052333083d688b7
Kioshi Okamoto <kjor29@gmail.com>
2021-03-22 19:44:31 -0500

    :fire: NEW: desarrollo inicial de skeleton

--- src/App.js
+++ src/App.js
@@ -1,5 +1,40 @@
+import './app.css';
+import { Skeleton } from './components';
+
 function App() {
-	return <div>Challenge 1</div>;
+	return (
+		<>
+			{/* EJEMPLO SKELETON */}
+			<div className="container">
+
+				{/* Linea individual */}
+				<Skeleton height="20px" />
+				{/* Skeleton con texto */}
+				<Skeleton>
+					<h1>text</h1>
+					<h1>text</h1>
+					<h1>text</h1>
+				</Skeleton>
+
+				{/* circulo con parametros */}
+				<Skeleton type="CIRCLE" size={5} />
+				{/* Circulo sin parametros */}
+				<Skeleton type="CIRCLE" />
+
+				
+				{/* Circulo al lado de parrafo */}
+				<div style={{ display: 'flex', width:'100%' }}>
+				<Skeleton type="CIRCLE" size={5} />
+				<div style={{width:'100%'}}>
+					<Skeleton height="20px" />
+					<Skeleton height="20px" />
+				</div>
+				
+			</div>
+			</div>
+			
+		</>
+	);
 }
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export { default as Button } from './Button/Button';
+export { default as Drawer} from './Drawer/Drawer';
+export { default as Input} from './Input/Input';
+export { default as Modal} from './Modal/Modal';
+export { default as Skeleton} from './Skeleton/Skeleton';
+export { default as Slider} from './Slider/Slider';
+export { default as Spinner} from './Spinner/Spinner';
+export { default as Switch} from './Switch/Switch';
