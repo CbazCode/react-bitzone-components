c2c72606ad0c4465f87d06d70a84e62ee7e64995
Sebastian Asis <sasisromero10@gmail.com>
2021-03-25 13:01:53 -0500

    :alembic: TEST: test en modal

--- src/tests/Modal/ModalHeader.test.js
+++ src/tests/Modal/ModalHeader.test.js
@@ -1,5 +1,5 @@
 import { ModalHeader } from "../../components/Modal/ModalElements";
-
+import renderer from 'react-test-renderer';
 //import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { shallow } from 'enzyme';
@@ -10,6 +10,13 @@ describe('Pruebas en el componente <ModalHeader/>', () => {
     test('debe renderizarse el componente correctamente', () => {
         const wrapper = shallow(<ModalHeader />);
         expect(wrapper).toMatchSnapshot();
+    })
+
+    test('debe tener los estilos', () => {
+        const wrapper = renderer
+      .create(<ModalHeader/>)
+      .toJSON();
+      expect(wrapper).toHaveStyleRule('font-size', '2rem');
     })
     
 })
--- src/tests/Modal/ModalWrapper.test.js
+++ src/tests/Modal/ModalWrapper.test.js
@@ -1,5 +1,5 @@
 import {  ModalWrapper } from "../../components/Modal/ModalElements";
-
+import renderer from 'react-test-renderer';
 //import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { shallow } from 'enzyme';
@@ -11,5 +11,36 @@ describe('Pruebas en el componente <ModalWrapper/>', () => {
         const wrapper = shallow(<ModalWrapper />);
         expect(wrapper).toMatchSnapshot();
     })
+
+    test('debe renderizarse con los estilos por defecto', () => {
+        const wrapper = renderer
+        .create(<ModalWrapper/>)
+        .toJSON();
+        expect(wrapper).toHaveStyleRule('width', '400px');
+        expect(wrapper).toHaveStyleRule('box-shadow', '0 5px 16px rgba(0,0,0,0.2)');
+        expect(wrapper).toHaveStyleRule('background', '#fff');
+        expect(wrapper).toHaveStyleRule('color', '#000');
+        expect(wrapper).toHaveStyleRule('display', 'grid');
+        expect(wrapper).toHaveStyleRule('grid-template-columns', '1fr');
+        expect(wrapper).toHaveStyleRule('position', 'relative');
+        expect(wrapper).toHaveStyleRule('z-index', '10');
+        expect(wrapper).toHaveStyleRule('border-radius', '10px');
+    })
+
+    test('debe renderizarse con el tamaño y grid debido si hay imagen', () => {
+        const wrapper = renderer
+        .create(<ModalWrapper withImage/>)
+        .toJSON();
+        expect(wrapper).toHaveStyleRule('width', '850px');
+        expect(wrapper).toHaveStyleRule('box-shadow', '0 5px 16px rgba(0,0,0,0.2)');
+        expect(wrapper).toHaveStyleRule('background', '#fff');
+        expect(wrapper).toHaveStyleRule('color', '#000');
+        expect(wrapper).toHaveStyleRule('display', 'grid');
+        expect(wrapper).toHaveStyleRule('grid-template-columns', '1fr 1fr');
+        expect(wrapper).toHaveStyleRule('position', 'relative');
+        expect(wrapper).toHaveStyleRule('z-index', '10');
+        expect(wrapper).toHaveStyleRule('border-radius', '10px');
+    })
+    
     
 })
