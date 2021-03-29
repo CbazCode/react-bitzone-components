af2081c8d848b4560c21d38e552aabbc37e2e2bc
Kioshi Okamoto <kjor29@gmail.com>
2021-03-29 17:42:26 -0500

    :construction: FIX:

--- styleguide.config.js
+++ styleguide.config.js
@@ -9,17 +9,18 @@ module.exports = {
           content: 'docs/ui.md',
           components: [
             'src/components/Button/[A-Z]*.js',
-            'src/components/Drawer/[A-Z]*.js',
-            'src/components/Input/[A-Z]*.js',
             'src/components/Modal/Modal.js',
-            'src/components/Skeleton/[A-Z]*.js',
-            'src/components/Slider/[A-Z]*.js',
             'src/components/Spinner/Spinner.js',
             'src/components/Switch/Switch.js',
+            'src/components/Slider/[A-Z]*.js',
+            'src/components/Drawer/[A-Z]*.js',
+            'src/components/Input/[A-Z]*.js',
+            'src/components/Skeleton/[A-Z]*.js',
         ],
           exampleMode: 'collapse', // 'hide' | 'collapse' | 'expand'
           usageMode: 'collapse' // 'hide' | 'collapse' | 'expand'
         }
       ]
 }
+
 
